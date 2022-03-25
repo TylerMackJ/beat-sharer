605488d4b586d14cf1dfdb5727867a55da7902e6
Tyler Johnson <tylermackj@gmail.com>
2022-03-24 22:53:17 -0500

    Added Icon

--- combined/src/main.rs
+++ combined/src/main.rs
@@ -14,9 +14,17 @@ extern crate dotenv_codegen;
 fn main() {
     dotenv::dotenv().ok();
 
+    let mut bytes = include_bytes!("icon.png").to_vec();
+    let mut im = image::io::Reader::new(std::io::Cursor::new(&mut bytes));
+    im.set_format(image::ImageFormat::Png);
+
     let app = BeatSharerApp::default();
-    //let native_options = eframe::NativeOptions::default();
     let native_options = eframe::NativeOptions {
+        icon_data: Some(eframe::epi::IconData {
+            rgba: im.decode().unwrap().to_rgba8().into_raw(),
+            width: 400,
+            height: 400,
+        }),
         resizable: false,
         ..Default::default()
     };
