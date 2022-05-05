80887ef66ce879542f9861e68f5afe4212bd854f
lindon <53193460+LindonKelley@users.noreply.github.com>
2022-05-05 01:25:23 -0500

    idiomatic update to db.rs renamed `id` to `index`

--- combined/src/api/db.rs
+++ combined/src/api/db.rs
@@ -1,29 +1,19 @@
 use crate::api::*;
 use crate::util::StringUtils;
 
-macro_rules! check_id {
-    ($id: expr) => {
-        if $id.parse::<u8>().is_err() {
-            return Err(APIErr::InvalidID);
-        }
-    };
-}
-
 const DB_ADDR: &str = "https://beat-sharer-default-rtdb.firebaseio.com";
 
-pub fn get_list(id: String) -> Result<Vec<String>, APIErr> {
-    check_id!(id);
+pub fn get_list(index: String) -> Result<Vec<String>, APIErr> {
+    check_index(&index)?;
 
     let auth = dotenv!("secret");
-    let addr = format!("{}/{}.json?auth={}", DB_ADDR, id, auth);
-
-    let mut contents = match reqwest::blocking::get(addr) {
-        Ok(con) => con.text().unwrap(),
-        Err(_) => return Err(APIErr::ReqwestFailed),
-    };
+    let addr = format!("{}/{}.json?auth={}", DB_ADDR, index, auth);
+    let response = reqwest::blocking::get(addr)?;
+    // can't use From here since that would just map to APIErr::ReqwestFailed
+    let mut contents = response.text().map_err(|_| APIErr::InvalidText)?;
 
     if contents == "null" {
-        return Err(APIErr::IDNotFound);
+        return Err(APIErr::IndexNotFound);
     }
 
     contents = contents.substring(1, contents.chars().count() - 2);
@@ -35,57 +25,50 @@ pub fn get_list(id: String) -> Result<Vec<String>, APIErr> {
     Ok(list)
 }
 
-pub fn put_list(id: String, list: String) -> Result<(), APIErr> {
-    check_id!(id);
+pub fn put_list(index: String, list: String) -> Result<(), APIErr> {
+    check_index(&index)?;
 
     let client = reqwest::blocking::Client::new();
-    return match client
+    client
         .put(format!(
             "{}/{}.json?auth={}",
             DB_ADDR,
-            id,
+            index,
             dotenv!("secret")
         ))
         .json(&list)
-        .send()
-    {
-        Ok(_) => Ok(()),
-        Err(_) => Err(APIErr::ReqwestFailed),
-    };
+        .send()?;
+    Ok(())
+}
+
+/// returns Ok if index is parsable to a u8, else API::InvalidIndex
+#[inline(always)]
+fn check_index(index: &String) -> Result<(), APIErr> {
+    index.parse::<u8>()?;
+    Ok(())
 }
 
 fn get_index() -> Result<u8, APIErr> {
     let auth = dotenv!("secret");
     let addr = format!("{}/index.json?auth={}", DB_ADDR, auth);
-    let contents = match reqwest::blocking::get(addr) {
-        Ok(con) => con.text().unwrap(),
-        Err(_) => return Err(APIErr::ReqwestFailed),
-    };
+    let response = reqwest::blocking::get(addr)?;
+    // can't use From here since that would just map to APIErr::ReqwestFailed
+    let contents = response.text().map_err(|_| APIErr::InvalidText)?;
 
     Ok(contents.parse::<u8>().unwrap())
 }
 
 fn put_index(index: u8) -> Result<(), APIErr> {
     let client = reqwest::blocking::Client::new();
-    let res = client
+    client
         .put(format!("{}/index.json?auth={}", DB_ADDR, dotenv!("secret")))
         .json(&index.to_string())
-        .send();
-
-    if res.is_err() {
-        return Err(APIErr::ReqwestFailed);
-    }
-
+        .send()?;
     Ok(())
 }
 
 pub fn get_and_inc_index() -> Result<u8, APIErr> {
-    let index = match get_index() {
-        Ok(i) => i,
-        Err(e) => return Err(e),
-    };
-    match put_index(index + 1) {
-        Ok(()) => Ok(index),
-        Err(e) => Err(e),
-    }
+    let index = get_index()?;
+    put_index(index.wrapping_add(1))?;
+    Ok(index)
 }
--- combined/src/api/mod.rs
+++ combined/src/api/mod.rs
@@ -1,9 +1,25 @@
+use std::num::ParseIntError;
+use reqwest::Error;
+
 pub mod bsaber;
 pub mod db;
 
 #[derive(Debug)]
 pub enum APIErr {
-    InvalidID,
-    IDNotFound,
+    InvalidIndex,
+    IndexNotFound,
     ReqwestFailed,
+    InvalidText,
+}
+
+impl From<reqwest::Error> for APIErr {
+    fn from(_: Error) -> Self {
+        APIErr::ReqwestFailed
+    }
+}
+
+impl From<ParseIntError> for APIErr {
+    fn from(_: ParseIntError) -> Self {
+        APIErr::InvalidIndex
+    }
 }
